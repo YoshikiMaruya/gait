d81eea5b9b77c3d7835ac666bc19a1d037d5201e
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-10-17 13:55:13 +0900

    create the model

--- GEINet/geinet.py
+++ GEINet/geinet.py
@@ -1,56 +1,34 @@
+from cv2 import resize
 import torch
 from torch import nn, optim
 from torch.optim import optimizer
 from torch.utils.data import (
   DataLoader
 )
+from torchvision.transforms.transforms import Resize
 import tqdm
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 import numpy as np
 
-train_images = ImageFolder(
-  "../CASIA-B_train",
-  transform=transforms.Compose([
-    transforms.RandomCrop(32),
-    transforms.ToTensor()
-  ])
-)
-
-test_images = ImageFolder(
-  "../CASIA-B_test",
-  transform=transforms.Compose([
-    transforms.RandomCrop(32),
-    transforms.ToTensor()
-  ])
-)
-
-train_loader = DataLoader(
-  train_images, batch_size=18, shuffle=True
-)
-
-test_loader = DataLoader(
-  test_images, batch_size=18, shuffle=True
-)
-
 class GEINet(nn.Module):
   def __init__(self):
-      super(GEINet, self).__init__()
-      self.relu = nn.ReLU()
-      self.softmax = nn.Softmax()
-      self.pool1 = nn.MaxPool2d(2, stride=2)
-      self.conv1 = nn.Conv2d(3, 25, 5)
-      self.norm1 = nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=1.0)
-      self.pool2 = nn.MaxPool2d(3, stride=2)
-      self.conv2 = nn.Conv2d(25, 45, 6)
-      self.norm2 = nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=1.0)
+    super(GEINet, self).__init__()
+    self.relu = nn.ReLU()
+    self.softmax = nn.Softmax()
+    self.pool1 = nn.MaxPool2d(2, stride=2)
+    self.conv1 = nn.Conv2d(3, 18, 7)
+    self.norm1 = nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=1.0)
+    self.pool2 = nn.MaxPool2d(2, stride=2)
+    self.conv2 = nn.Conv2d(18, 45, 5)
+    self.norm2 = nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=1.0)
 
-      self.fc1 = nn.Linear(18*45, 1024)
-      self.fc2 = nn.Linear(1024, 74)
+    self.fc1 = nn.Linear(45*56*76, 1024)
+    self.fc2 = nn.Linear(1024, 74)
 
-      self.dropout = nn.Dropout2d(0.5)
+    self.dropout = nn.Dropout2d(0.5)
 
-  def forward(self, l):
+  def forward(self, l, fc2_outputs):
     l = self.conv1(l)
     l = self.relu(l)
     l = self.pool1(l)
@@ -59,58 +37,14 @@ class GEINet(nn.Module):
     l = self.relu(l)
     l = self.pool2(l)
     l = self.norm2(l)
+    l = l.view(l.size()[0], -1)
     l = self.fc1(l)
     l = self.relu(l)
     l = self.dropout(l)
     l = self.fc2(l)
+    fc2_outputs = l
     l = self.softmax(l)
-    return l
+    return l, fc2_outputs
 
-GEINet = GEINet()
-
-def eval_net(GEINet, data_loader, device="cpu"):
-  GEINet.eval()
-  ys = []
-  ypreds = []
-  for x, y in data_loader:
-    x = x.to(device)
-    y = y.to(device)
-    with torch.no_grad():
-      _, y_pred = GEINet(x).max(1)
-    ys.append(y)
-    ypreds.append(ys)
-
-    acc = (ys == ypreds).float().sum() / len(ys)
-    return acc.item()
-
-def train_net(GEINet, train_loader, test_loader, loss_fn=nn.CrossEntropyLoss(), n_iter=10, device="cpu"):
-  train_losses = []
-  train_acc = []
-  val_acc = []
-  optimizer = torch.optim.Adam(GEINet.parameters())
-  for epoch in range(n_iter):
-    running_loss = 0.0
-    GEINet.train()
-    n = 0
-    n_acc = 0
-    for i, (xx, yy) in tqdm.tqdm(enumerate(train_loader), total = len(train_loader)):
-      xx = xx.to(device)
-      yy = yy.to(device)
-      h = GEINet(xx)
-      loss = loss_fn(h, yy)
-      optimizer.zero_grad()
-      loss.backward()
-      optimizer.step()
-      running_loss += loss.item()
-      n += len(xx)
-      _, y_pred = h.max(1)
-      n_acc += (yy == y_pred).float().sum().item()
-
-    train_losses.append(running_loss / i)
-    train_acc.append(n_acc / n)
-    val_acc.append(eval_net(GEINet, test_loader, device))
-
-    print(epoch, train_losses[-1], train_acc[-1], val_acc[-1], flush=True)
-
-# GEINet.to("cuda:0")
-train_net(GEINet, train_loader, test_loader, n_iter=20, device="cpu")
+gei_l, gei_fc2_outputs = GEINet()
+print(gei_l.forward())
