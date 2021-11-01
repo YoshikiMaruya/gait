e985f28d397965ba9bd411ecdcc2d666733326f3
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-11-01 23:39:16 +0900

    reloaded train file

--- GEINet/train.py
+++ GEINet/train.py
@@ -1,5 +1,4 @@
-import torch
-from torch import nn, optim
+from torch import nn, optim, linalg
 from torch.optim import optimizer
 from torch.utils.data import (
   DataLoader
@@ -8,12 +7,18 @@ import tqdm
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 import numpy as np
-from model import geinet as gei
+import matplotlib.pyplot as plt
+import model.geinet
+
+BATCH_SIZE = 31
+WEIGHT_DECAY = 0.005
+LEARNING_RATE = 0.0001
+EPOCH = 100
 
 train_images = ImageFolder(
   "../CASIA-B_train",
   transform=transforms.Compose([
-    transforms.RandomCrop(32),
+    transforms.Resize((240, 320)),
     transforms.ToTensor()
   ])
 )
@@ -21,65 +26,26 @@ train_images = ImageFolder(
 test_images = ImageFolder(
   "../CASIA-B_test",
   transform=transforms.Compose([
-    transforms.RandomCrop(32),
+    transforms.Resize((240, 320)),
     transforms.ToTensor()
   ])
 )
 
 train_loader = DataLoader(
-  train_images, batch_size=32, shuffle=True
+  train_images, batch_size=BATCH_SIZE, shuffle=True
 )
 
 test_loader = DataLoader(
-  test_images, batch_size=32, shuffle=True
+  test_images, batch_size=BATCH_SIZE, shuffle=False
 )
 
-gei = gei.GEINet()
-print(gei)
+gei = model.geinet.GEINet()
 
-def eval_net(GEINet, data_loader, device="cpu"):
-  GEINet.eval()
-  ys = []
-  ypreds = []
-  for x, y in data_loader:
-    x = x.to(device)
-    y = y.to(device)
-    with torch.no_grad():
-      _, y_pred = GEINet(x).max(1)
-    ys.append(y)
-    ypreds.append(ys)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(gei.parameters(), lr=0.0001, momentum=0.9, weight_decay=0.005)
 
-    acc = (ys == ypreds).float().sum() / len(ys)
-    return acc.item()
 
-def train_net(GEINet, train_loader, test_loader, loss_fn=nn.CrossEntropyLoss(), n_iter=10, device="cpu"):
-  train_losses = []
-  train_acc = []
-  val_acc = []
-  optimizer = torch.optim.Adam(GEINet)
-  for epoch in range(n_iter):
-    running_loss = 0.0
-    GEINet.train()
-    n = 0
-    n_acc = 0
-    for i, (xx, yy) in tqdm.tqdm(enumerate(train_loader), total = len(train_loader)):
-      xx = xx.to(device)
-      yy = yy.to(device)
-      h = GEINet(xx)
-      loss = loss_fn(h, yy)
-      optimizer.zero_grad()
-      loss.backward()
-      optimizer.step()
-      running_loss += loss.item()
-      n += len(xx)
-      _, y_pred = h.max(1)
-      n_acc += (yy == y_pred).float().sum().item()
-
-    train_losses.append(running_loss / i)
-    train_acc.append(n_acc / n)
-    val_acc.append(eval_net(GEINet, test_loader, device))
-
-    print(epoch, train_losses[-1], train_acc[-1], val_acc[-1], flush=True)
-
-# GEINet.to("cuda:0")
-train_net(GEINet, train_loader, test_loader, n_iter=20, device="cpu")
+for (inputs, labels) in train_loader:
+  optimizer.zero_grad()
+  gei_l, gei_feature = gei.forward(inputs)
+  print(gei_feature.size())
