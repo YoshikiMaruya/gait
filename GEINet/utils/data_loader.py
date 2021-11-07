9983c8accec922262e95749815e97c66922ab6a0
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-11-07 19:14:21 +0900

    gei image split train and test

--- GEINet/utils/data_loader.py
+++ GEINet/utils/data_loader.py
@@ -1,27 +1,19 @@
 import os
-import os.path as osp
-import torchvision.transforms.functional as TF
-import numpy as np
+import glob
+from PIL import Image
 
-def load_data(dataset_path):
-  train_data = list()
-  test_data = list()
+train_dir = '/home/yoshimaru/gait/GEINet/gei_image/train_gei'
+test_dir = '/home/yoshimaru/gait/GEINet/gei_image/test_gei'
+os.makedirs(train_dir, exist_ok=True)
+os.makedirs(test_dir, exist_ok=True)
 
-  prove_dict = {'CASIA': [['nm-05', 'nm-06'], ['bg-01', 'bg-02'], ['cl-01', 'cl-02']]}
-  gallery_dict = {'CASIA': ['nm-03', 'nm-04', 'nm-05', 'nm-06']}
+files = glob.glob('/home/yoshimaru/gait/GEI/*.png')
 
-  # for value in prove_dict.values():
-  #   print(value[0][0])
-
-  for _data in sorted(list(os.listdir(dataset_path))):
-    if _data[:3] >= "075":
-      test_data.append(_data)
-    train_data.append(_data)
-
-  
-
-# def main():
-#   # print(sorted(list(os.listdir("/home/yoshimaru/gait/CASIA-B_train"
-#   load_data("/home/yoshimaru/gait/GEI")
-# if __name__ == "__main__":
-#   main()
+for f in sorted(files):
+  img = Image.open(f)
+  root, ext = os.path.splitext(f)
+  basename = os.path.basename(root)
+  if f[25:28] < "075":
+    img.save(os.path.join(train_dir, basename + ext))
+  if f[25:28] >= "075":
+    img.save(os.path.join(test_dir, basename + ext))
