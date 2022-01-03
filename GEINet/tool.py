7f7efc349525c78dbdc4bfb784e8d00bcd67e636
YoshikiMaruya <yoshimaru0514@gmail.com>
2022-01-03 20:47:22 +0900

    coding calculate the feature vector

--- GEINet/tool.py
+++ GEINet/tool.py
@@ -1,5 +1,13 @@
-from torch import nn
+from PIL import Image
+import torch
+import numpy as np
 
-def feature_extract_model(model):
-  res = nn.Sequential()
-  
+def calc_feature(image, transform, model):
+  image = Image.open(image).convert("RGB")
+  image = transform(image)
+  image = image.unsqueeze(0)
+  # if torch.cuda.is_available():
+  #   image = image.to(device)
+  _, feature = model.forward(image)
+
+  return feature
